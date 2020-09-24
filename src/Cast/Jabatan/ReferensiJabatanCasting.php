33d24f9e61048d6225ea3e735f4aa1fd16cf88ae
Denma Syarikin <denmasyarikin@gmail.com>
2020-09-24 11:14:58 +0700

    [UPDATE] small enhancement

---
+++ src/Cast/Jabatan/ReferensiJabatanCasting.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace SotkClient\Cast\Jabatan;
+
+use InvalidArgumentException;
+use SotkClient\Cast\BaseCasting;
+use SotkClient\Models\Jabatan\JabatanFungsional;
+use SotkClient\Models\Jabatan\JabatanPelaksana;
+use SotkClient\Models\Jabatan\JabatanPolitik;
+use SotkClient\Models\Jabatan\JabatanStruktural;
+use SotkClient\Models\Jabatan\JabatanTugasTambahan;
+use SotkClient\Models\Jabatan\ReferensiJabatanContract;
+
+class ReferensiJabatanCasting extends BaseCasting
+{
+    /**
+     * cast to model
+     *
+     * @var \Illuminate\Database\Eloquent\Model
+     */
+    protected $model = ReferensiJabatanContract::class;
+
+    /**
+     * create data
+     *
+     * @param array $data
+     * @return \Illuminate\Database\Eloquent\Model
+     */
+    protected function createData(array $data)
+    {
+        switch ($data['jenis']) {
+            case 'struktural':
+                return new JabatanStruktural($data);
+            break;
+            case 'fungsional':
+                return new JabatanFungsional($data);
+            break;
+            case 'pelaksana':
+                return new JabatanPelaksana($data);
+            break;
+            case 'politik':
+                return new JabatanPolitik($data);
+            break;
+            case 'tugas_tambahan':
+                return new JabatanTugasTambahan($data);
+            break;
+        }
+
+        throw new InvalidArgumentException("{$data['jenis']} not valid jenis jabaan");
+    }
+
+    /**
+     * custom method casting
+     *
+     * @param array $data
+     * @return \Illuminate\Database\Eloquent\Model
+     */
+    public static function setReferensiCasting ($data)
+    {
+        return (new static)->createData($data);
+    }
+}
---
+++ src/Models/Jabatan/ReferensiJabatanContract.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace SotkClient\Models\Jabatan;
+
+interface ReferensiJabatanContract
+{
+
+}
